349497967e6d4b6d80d460e75f17151e88a6f7cf
Talon Holton <jholton2019@my.fit.edu>
2022-09-14 14:29:43 -0400

    Started work on data collection code

---
+++ DataCollection/recorder.py
@@ -0,0 +1,88 @@
+import serial
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+# random constants for handshake
+HANDSHAKE_START = 0x7E
+HANDSHAKE_RESPONSE = 0x7F
+START_CODE = 0x80
+
+
+def main(port):
+    # open the provided port at 115200 baud.
+    ser = serial.Serial(port, 115200)
+
+    # send the handshake start byte, and wait for the response.
+    ser.write(bytes([HANDSHAKE_START]))
+    response = ser.read()
+
+    # if the response is not the handshake response, exit.
+    if response != bytes([HANDSHAKE_RESPONSE]):
+        print("Incorrect Handshake response. Please restart the arduino and try again.")
+        sys.exit(1)
+
+    # prompt user for the duration of the recording.
+    duration = int(input("Enter the duration of the recording in seconds: "))
+    print("Recording for {} seconds...".format(duration))
+
+    # convert to milliseconds, and send the start code.
+    millis_duration = duration * 1000
+    # send the 4 byte duration to the arduino.
+    ser.write(bytes([START_CODE, millis_duration >> 24,
+              millis_duration >> 16, millis_duration >> 8, millis_duration]))
+
+    samples = list()
+    sample_times = list()
+    start_time = time.time()
+    while time.time() - start_time < duration:
+        # read the 4 bytes from the arduino.
+        data = ser.read(4)
+        # convert the 4 bytes to a 32 bit integer.
+        sample = int.from_bytes(data, byteorder='big', signed=True)
+        # append the sample to the list of samples.
+        samples.append(sample)
+        sample_times.append(time.time() - start_time)
+
+    print("Done recording.")
+
+    # save the samples to a csv file.
+    with open("samples.csv", "w") as f:
+        for sample, sample_time in zip(samples, sample_times):
+            f.write("{},{}\n".format(sample_time, sample))
+
+    print("Saved samples to samples.csv")
+
+    # plot the samples.
+    fig, ax = plt.subplots(3, 1)
+    ax[0].plot(sample_times, samples)
+    ax[0].set(xlabel='time (s)', ylabel='sample',
+           title=f"Samples Recorded on {port} for {duration} seconds")
+    ax[0].grid()
+
+    # plot the difference between samples.
+    ax[1].plot(sample_times[1:], np.diff(samples))
+    ax[1].set(xlabel='time (s)', ylabel='sample',
+              title=f"Sample Differences")
+    ax[1].grid()
+
+    # plot the running sum of the samples.
+    ax[2].plot(sample_times, np.cumsum(samples))
+    ax[2].set(xlabel='time (s)', ylabel='sample',
+                title=f"Running Sum of Samples")
+    ax[2].grid()
+
+    fig.savefig("samples.png")
+
+    print("Saved plot to samples.png")
+    plt.show()
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    # ensure that the user has provided a port
+    if len(sys.argv) < 2:
+        print('Usage: python3 recorder.py <port>')
+        sys.exit(1)
+    main(sys.argv[1])
