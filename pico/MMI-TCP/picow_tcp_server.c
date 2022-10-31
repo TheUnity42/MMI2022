e9ef00149d1c8b8237a0fc74228f080d6529edd2
Talon Holton <jholton2019@my.fit.edu>
2022-10-31 14:24:45 -0400

    docs for pico tcp

--- pico/MMI-TCP/picow_tcp_server.c
+++ pico/MMI-TCP/picow_tcp_server.c
@@ -4,6 +4,12 @@
 #include "tcp_server.h"
 
 #define TCP_PORT 4242
+#define SETUP_ERROR_MSG "Unable to setup TCP server\n"
+#define HANDLE_SETUP_ERROR(cond)                                                                   \
+	if (cond) {                                                                                    \
+		printf(SETUP_ERROR_MSG);                                                                   \
+		goto prog_end;                                                                             \
+	}
 
 void on_recieve(struct tcp_server *server, uint16_t len);
 
@@ -19,27 +25,23 @@ int main() {
 
 	server->on_recv = &on_recieve;
 
-	if (!server) {
-		printf("Unable to create server\n");
-		return 1;
-	}
+	HANDLE_SETUP_ERROR(!server)
 
-	if (tcp_server_open(server, 4242)) {
-		printf("Error creating TCP Server\n");
-	}
+	HANDLE_SETUP_ERROR(tcp_server_open(server, 4242))
 
 	while (1) {
 		// do nothing
 		sleep_ms(1000);
 	}
 
+prog_end:
 	wifi_close();
 	return 0;
 }
 
 void on_recieve(struct tcp_server *server, uint16_t len) {
 	// log message
-    printf("Server recieved: %s\n", (char *)server->rx_buffer);
-    // echo message
-    tcp_write_immediate(server, server->rx_buffer, len);
+	printf("Server recieved: %s\n", (char *)server->rx_buffer);
+	// echo message
+	tcp_write_immediate(server, server->rx_buffer, len);
 }
