47d218b83e18a183dfc82c7199a891cbe6224e52
Talon Holton <jholton2019@my.fit.edu>
2023-03-31 12:07:57 -0400

    CleanUp

--- udp/animatedTCPReader.py
+++ udp/animatedTCPReader.py
@@ -9,23 +9,25 @@ from matplotlib import pyplot as plt
 
 bufferSize  = 1024
 
-def create_close_handler(sensors):
+def create_close_handler(boards):
     def handle_close_event(evt):
         # shutdown
-        for sensor in sensors:
-            sensor.close()
+        for board in boards:
+            board.close()
         sys.exit(0)
     return handle_close_event
 
-class Sensor(object):    
+class Boards(object):    
     def __init__(self, name, addr, file_handle=None):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(addr)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 500)
         self.file_handle = file_handle
         self.name = name
+
     def close(self):
         self.sock.close()
+
     def __call__(self):
         data = self.sock.recv(bufferSize).decode('utf-8')
         x = []
@@ -47,19 +49,21 @@ class Sensor(object):
     
 
 class SensorPlot:
-    def __init__(self, ax, keep):
+    def __init__(self, ax, board, keep):
         self.x = np.linspace(-keep, 0, keep)
         self.y0 = [0]*keep
         self.y1 = [0]*keep
         self.y2 = [0]*keep
         self.keep = keep
         self.ax = ax
+        self.board = board
         
-        self.plot0 = ax[0].plot(self.x, self.y0, color='r', label='Sensor 0')
-        self.plot1 = ax[0].plot(self.x, self.y1, color='g', label='Sensor 1')
-        self.plot2 = ax[0].plot(self.x, self.y2, color='b', label='Sensor 2')
+        self.plot0 = ax[0].plot(self.x, self.y0, color='r', label=f'{board.name}: Sensor 0')
+        self.plot1 = ax[0].plot(self.x, self.y1, color='g', label=f'{board.name}: Sensor 1')
+        self.plot2 = ax[0].plot(self.x, self.y2, color='b', label=f'{board.name}: Sensor 2')
     
-    def update_from_sensor(self, n_x, n_y0, n_y1, n_y2):
+    def __call__(self):
+        n_x, n_y0, n_y1, n_y2 = self.board()
         #new_x = x + new_x
         n_y0 = self.y0 + n_y0
         n_y1 = self.y1 + n_y1
@@ -89,21 +93,20 @@ def main(ip, port, keep, file_name):
     addr0 = (str(ip), int(port))
     addr1 = (str(ip), int(port)+1)
 
-
     keep = int(keep) # number of points to keep
 
     file = open(file_name, "w+")
 
-    sensor0 = Sensor("Sensor0", addr0, file)
-    sensor1 = Sensor("Sensor1", addr1, file)
+    board0 = Boards("Board0", addr0, file)
+    board1 = Boards("Board1", addr1, file)
 
     fig, ax = plt.subplots(1, 2, figsize=(12, 6))
     fig.tight_layout(pad=3)
 
-    s_plot0 = SensorPlot(ax, keep)
-    s_plot1 = SensorPlot(ax, keep)
+    s_plot0 = SensorPlot(ax, board0, keep)
+    s_plot1 = SensorPlot(ax, board1, keep)
 
-    fig.canvas.mpl_connect('close_event', create_close_handler([sensor0]))
+    fig.canvas.mpl_connect('close_event', create_close_handler([board0, board1]))
 
     ax[0].set_title(f"Sensor Data from {addr0[0]}:{addr0[1]}-{addr1[0]}")
     ax[0].legend()
@@ -120,14 +123,11 @@ def main(ip, port, keep, file_name):
     plt.pause(0.1)
 
     while True:
-        # n_x, n_y0, n_y1, n_y2 = sensor0()
-        # s1_x, s1_y0, s1_y1, s1_y2 = sensor1()
-
         # restore background
         fig.canvas.restore_region(background)
         # redraw the plots
-        s_plot0.update_from_sensor(sensor0())
-        s_plot1.update_from_sensor(sensor1())
+        s_plot0()
+        s_plot1()
 
         # fill in the axes rectangle
         fig.canvas.blit(ax[0].bbox)
@@ -139,5 +139,4 @@ if __name__ == '__main__':
         main(sys.argv[1], int(sys.argv[2]), sys.argv[3], sys.argv[4])
     else:
         print("Usage: python3 plot.py <ip> <port> <graph_window> <file>")
-        # main('192.168.86.51', 4242, 100)
     sys.exit(0)
